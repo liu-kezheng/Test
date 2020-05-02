98840312acdf468839f0c168ce1159c1369ac3e8
liukezheng <1573524354@qq.com>
2020-05-02 11:53:00 +0800

    ha2

--- Test/src/com/sunreal/test/TestOne.java
+++ Test/src/com/sunreal/test/TestOne.java
@@ -9,13 +9,13 @@ public class TestOne {
 		System.out.println("第一次下拉");
 		System.out.println("提交测试");
 		ArrayList<String> list = new ArrayList<String>();
-		list.add("111");
+		list.add("456");
 		list.add("123");
 		ArrayList<String> list2 = new ArrayList<String>();
-		list.add("22aaa");
 		list.add("33bbb");
 		ArrayList<String> list3 = new ArrayList<String>();
 		list3.add("1");
 		System.out.println("哈哈哈哈哈哈");
+		System.out.println("哈哈哈哈哈哈2");
 	}
 }
