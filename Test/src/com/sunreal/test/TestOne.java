d7b0668ebbe79064c3df7723e63b6678705e21f6
liu-kezheng <62451860+liu-kezheng@users.noreply.github.com>
2020-05-02 11:33:03 +0800

    Update TestOne.java

--- Test/src/com/sunreal/test/TestOne.java
+++ Test/src/com/sunreal/test/TestOne.java
@@ -9,7 +9,7 @@ public class TestOne {
 		System.out.println("第一次下拉");
 		System.out.println("提交测试");
 		ArrayList<String> list = new ArrayList<String>();
-		list.add("1");
+		list.add("123");
 
 	}
 }
