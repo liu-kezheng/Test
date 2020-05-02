a530ac2bdc3c09121114c3d0337f827cca8a8ca6
liukezheng <1573524354@qq.com>
2020-05-02 15:15:48 +0800

    1

--- Test/src/com/sunreal/test/TestOne.java
+++ Test/src/com/sunreal/test/TestOne.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class TestOne {
 	public static void main(String[] args) {
-		System.out.println("解决分支问题");
+		System.out.println("解决分支问题1");
 		System.out.println("第一次下拉");
 		System.out.println("提交测试");
 		ArrayList<String> list = new ArrayList<String>();
