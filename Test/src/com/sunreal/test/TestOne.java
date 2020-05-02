b2f202d37fa6f27dde75b957bced65c6ba446c90
liukezheng <1573524354@qq.com>
2020-05-02 11:29:27 +0800

    ArrayList

--- Test/src/com/sunreal/test/TestOne.java
+++ Test/src/com/sunreal/test/TestOne.java
@@ -1,10 +1,14 @@
 package com.sunreal.test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestOne {
 	public static void main(String[] args) {
 		System.out.println("解决分支问题");
 		System.out.println("第一次下拉");
 		System.out.println("提交测试");
-		
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("1");
 	}
 }
