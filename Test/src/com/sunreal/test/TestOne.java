b8aaf3132ed8f27348c4785eceff86b9911bfba5
liukezheng <1573524354@qq.com>
2020-05-02 16:50:34 +0800

    11

--- Test/src/com/sunreal/test/TestOne.java
+++ Test/src/com/sunreal/test/TestOne.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class TestOne {
 	public static void main(String[] args) {
-		System.out.println("解决分zsd支问题  "   );
+		System.out.println("解决分zsd支问题  " +"asa" );
 		System.out.println("解决分zsd支问题");
 
 	}
