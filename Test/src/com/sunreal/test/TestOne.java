f29c3c0ca776d094d9e9d06c489dc2a99b206f53
liu-kezheng <62451860+liu-kezheng@users.noreply.github.com>
2020-05-02 15:18:14 +0800

    Update TestOne.java

--- Test/src/com/sunreal/test/TestOne.java
+++ Test/src/com/sunreal/test/TestOne.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class TestOne {
 	public static void main(String[] args) {
-		System.out.println("解决分支问题测试");
+		System.out.println("解决分支问题测试1");
 		System.out.println("第一次下拉");
 		System.out.println("提交测试");
 		ArrayList<String> list = new ArrayList<String>();
@@ -17,7 +17,7 @@ public class TestOne {
 		list.add("123");
 		ArrayList<String> list3 = new ArrayList<String>();
 		list3.add("1");
-		System.out.println("哈哈哈哈哈哈");
+		System.out.println("哈哈哈哈哈哈1");
 		System.out.println("哈哈哈哈哈哈2");
 		System.out.println("哈哈哈哈哈哈2234");
 		System.out.println("哈哈哈哈哈哈2236");
@@ -25,6 +25,6 @@ public class TestOne {
 		System.out.println("哈哈哈哈哈哈223");
 		System.out.println("aaa");
 		System.out.println("bbb");
-		System.out.println("a2a1a");
+		System.out.println("a2a1a1");
 	}
 }
